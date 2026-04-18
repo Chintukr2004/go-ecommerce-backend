361dc911ea3ed32cb8456bb955ff2b9e0a5db1f0
Chintukr2004 <chintukr1904@gmail.com>
2026-04-18 23:36:36 +0530

    Project setup with gin server and env config

---
+++ cmd/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status":  "ok",
+			"message": "server is runnig",
+		})
+	})
+
+	log.Println("server started on port ", port)
+
+	r.Run(":" + port)
+
+}
